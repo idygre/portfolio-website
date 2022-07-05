8daa52d066884f834c37184f4637998d9cc185c3
Greydi Lora <greydi.lora@yahoo.com>
2022-07-05 17:33:33 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hi</p>
+        <p>second</p>
         <a
           className="App-link"
           href="https://reactjs.org"
