c7955cb23fe5da0d74533c37e6c1a410f027c903
Greydi Lora <greydi.lora@yahoo.com>
2022-08-10 18:41:34 -0400

    Finished the footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,34 @@ import React from "react";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsLinkedin } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qkakx2c",
+        "template_0pdflbj",
+        form.current,
+        "KKQ5gd4JuUyZR28k8"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -29,7 +55,7 @@ const Contact = () => {
         </div>
         {/* END OF CONTACT OPTIONS */}
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input
             type="text"
             name="name"
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Greydi
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://www.linkedin.com/in/greydi-lora-62193519b/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a href="https://github.com/idygre" target="_blank">
+          <FaGithub />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
