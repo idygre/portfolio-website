0e6fde2228012f478b53c44cacb5dacea20119e2
Greydi Lora <greydi.lora@yahoo.com>
2022-08-14 19:45:16 -0400

    add project files

---
+++ src/components/projects/project1/Project1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Project1.css";
+
+const Project1 = () => {
+  return (
+    <div className="testing">
+      <p>This is the second page.</p>
+    </div>
+  );
+};
+
+export default Project1;
